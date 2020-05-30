be5eaf530c4b16fd3690b040550e4c6b8785ecf1
Gabriel Henrique Casachi <gcasachi@gmail.com>
2020-05-30 16:09:52 -0300

    Finalizado - Setor (BLL, DAL e MODEL)

---
+++ SistemaAlmoxerifado/CAMADAS/BLL/Setor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAlmoxerifado.CAMADAS.BLL {
+    public class Setor {
+        public List<MODEL.Setor> Select() {
+            DAL.Setor dalSetor = new DAL.Setor();
+            return dalSetor.Select();
+        }
+
+        public void Insert(MODEL.Setor setor) {
+            DAL.Setor dalSetor = new DAL.Setor();
+            dalSetor.Insert(setor);
+        }
+
+        public void Update(MODEL.Setor setor) {
+            DAL.Setor dalSetor = new DAL.Setor();
+            dalSetor.Update(setor);
+        }
+
+        public void Delete(int idSetor) {
+            DAL.Setor dalSetor = new DAL.Setor();
+            dalSetor.Delete(idSetor);
+        }
+    }
+}
