27f6a5bbfd8dbc16f7db56d28d3184c6734cbb17
Paulo Romano <paulo-romano_133@hotmail.com>
2020-05-30 14:51:06 -0300

    Criação classe Conexão

---
+++ SistemaAlmoxerifado/CAMADAS/DAL/Conexao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAlmoxerifado.CAMADAS.DAL {
+    class Conexao {
+
+        public static string getConexao() {
+            return @"Data Source=.;Initial Catalog=SISTEMA_ALMOXARIFADO;Integrated Security=True";
+        }
+    }
+}
