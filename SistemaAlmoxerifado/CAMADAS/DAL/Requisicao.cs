fab2500fe635fb30e53d08a8576d4187d2b5475a
Paulo Romano <paulo-romano_133@hotmail.com>
2020-05-30 22:37:45 -0300

    DAL e BLL da Requisicao

---
+++ SistemaAlmoxerifado/CAMADAS/DAL/Requisicao.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAlmoxerifado.CAMADAS.DAL {
+    class Requisicao {
+        private string strCon = Conexao.getConexao();
+
+        public List<MODEL.Requisicao> Select() {
+            List<MODEL.Requisicao> lstFornecedores = new List<MODEL.Requisicao>();
+
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "SELECT * FROM Fornecedor";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+
+            try {
+                conexao.Open();
+                SqlDataReader dados = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dados.Read()) {
+                    MODEL.Requisicao requisicao = new MODEL.Requisicao();
+
+                    requisicao.id = Convert.ToInt32(dados["id"].ToString());
+                    requisicao.produtoID = Convert.ToInt32(dados["produto_id"].ToString());
+                    requisicao.quantidade = Convert.ToInt32(dados["quantidade"].ToString());
+                    requisicao.data = Convert.ToDateTime(dados["data"].ToString());
+
+                    lstFornecedores.Add(requisicao);
+                }
+            }
+            catch {
+                Console.WriteLine("Erro no Select do Requisição");
+            }
+            finally {
+                conexao.Close();
+            }
+            return lstFornecedores;
+        }
+
+        public void Insert(MODEL.Requisicao requisicao) {
+            SqlConnection conexao = new SqlConnection(strCon);
+
+            string sql = "INSERT INTO Requisicao VALUES (@setor_id, @produto_id, @quantidade, @data);";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+
+            cmd.Parameters.AddWithValue("@setor_id", requisicao.setorID);
+            cmd.Parameters.AddWithValue("@produto_id", requisicao.produtoID);
+            cmd.Parameters.AddWithValue("@quantidade", requisicao.quantidade);
+            cmd.Parameters.AddWithValue("@data", requisicao.data);
+
+            try {
+                conexao.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch {
+                Console.WriteLine("Erro no Insert de Requisicao");
+            }
+            finally {
+                conexao.Close();
+            }
+        }
+
+        public void Update(MODEL.Requisicao requisicao) {
+            SqlConnection conexao = new SqlConnection(strCon);
+
+            string sql = "UPDATE Requisicao SET quantidade=@quantidade";
+            sql += "WHERE id=@id;";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+
+            cmd.Parameters.AddWithValue("@quantidade", requisicao.quantidade);
+
+            try {
+                conexao.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch {
+                Console.WriteLine("Erro sql atualizar Requisicao...");
+            }
+            finally {
+                conexao.Close();
+            }
+        }
+
+        public void Delete(int idRequisicao) {
+            SqlConnection conexao = new SqlConnection(strCon);
+
+            string sql = "DELETE FROM Requisicao WHERE id=@id;";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+
+            cmd.Parameters.AddWithValue("@id", idRequisicao);
+
+            try {
+                conexao.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch {
+                Console.WriteLine("Erro sql Remover Requisicao...");
+            }
+            finally {
+                conexao.Close();
+            }
+        }
+    }
+}
